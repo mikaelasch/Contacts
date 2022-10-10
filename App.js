2482fea83218339101d5582d0b352b632997523b
Mikaela Schröder <mikaela.schroder@myy.haaga-helia.fi>
2022-10-10 17:44:26 +0300

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,14 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,  {useState } from 'react'
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import * as Contacts from 'expo-contacts' 
+
+
 
 export default function App() {
+ const [contact, setContact] = useState({})
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync(
+        {fields: [Contacts.Fields.PhoneNumbers]}
+      )
+      if ( data.length > 0)  {
+        setContact(data)
+      }
+    }
+  }
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 5,
+          width: "80%",
+          backgroundColor: "#fff",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item})=>
+          <View>
+            <Text>{item.firstName} {item.phoneNumbers && item.phoneNumbers[0] && item.phoneNumbers[0].number}</Text>
+          </View>}
+          data={contact}
+          ItemSeparatorComponent={listSeparator}
+         />
+      <Button title="Get Contact"onPress={getContacts} />
     </View>
   );
-}
+  }
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +53,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
